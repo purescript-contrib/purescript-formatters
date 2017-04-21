59fc87d38ae5ba5b18b42245725434f95c533019
Irakli Safareli <i.safareli@gmail.com>
2017-04-21 19:46:58 +0400

    include error position in error string of parser result

--- src/Data/Formatter/Parser/Utils.purs
+++ src/Data/Formatter/Parser/Utils.purs
@@ -1,16 +1,26 @@
-module Data.Formatter.Parser.Utils where
+module Data.Formatter.Parser.Utils
+  ( oneOfAs
+  , runP
+  ) where
 
 import Prelude
 
 import Data.Tuple (Tuple(..))
-import Text.Parsing.Parser as P
+import Text.Parsing.Parser (ParserT, Parser, runParser, ParseError, parseErrorMessage, parseErrorPosition)
+import Text.Parsing.Parser.Pos (Position(..))
 import Text.Parsing.Parser.Combinators as PC
 import Data.Bifunctor (lmap)
 import Data.Foldable (class Foldable)
 import Data.Either (Either)
 
-oneOfAs :: ∀ c s m f a b. Functor f => Foldable f => Monad m => (a -> P.ParserT s m b) -> f (Tuple a c) -> P.ParserT s m c
+oneOfAs :: ∀ c s m f a b. Functor f => Foldable f => Monad m => (a -> ParserT s m b) -> f (Tuple a c) -> ParserT s m c
 oneOfAs p xs = PC.choice $ (\(Tuple s r) -> p s $> r) <$> xs
 
-runP :: ∀ a. P.Parser String a → String → Either String a
-runP p s = lmap P.parseErrorMessage $ P.runParser s p
+runP :: ∀ s a. Parser s a → s → Either String a
+runP p s = lmap printError $ runParser s p
+
+printError :: ParseError -> String
+printError err = parseErrorMessage err  <> "@" <> (printPosition $ parseErrorPosition err)
+
+printPosition :: Position -> String
+printPosition (Position {line, column}) = show line <> ":" <> show column
