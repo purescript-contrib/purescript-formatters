9545498b378ce9e60146744b9adb7f980d2d98ef
nathan <nathan.wilson@marquetteenergyanalytics.com>
2020-08-21 21:08:39 -0400

    document why the showNumberAsInt function exists

--- src/Data/Formatter/Number.js
+++ src/Data/Formatter/Number.js
@@ -1,2 +1,5 @@
 
+// converts a number to a string of the nearest integer _without_ appending ".0" (like `show` for `Number`) or
+// clamping to +/- 2 billion (like when working with `Int`). This is important for performance compared to other
+// means of showing an integer potentially larger than +/- 2 billion.
 exports.showNumberAsInt = function (n) { return Math.round(n).toString(); }
--- src/Data/Formatter/Number.purs
+++ src/Data/Formatter/Number.purs
@@ -82,6 +82,9 @@ formatParser = do
     , abbreviations: isJust abbreviations
     }
 
+-- converts a number to a string of the nearest integer _without_ appending ".0" (like `show` for `Number`) or
+-- clamping to +/- 2 billion (like when working with `Int`). This is important for performance compared to other
+-- means of showing an integer potentially larger than +/- 2 billion.
 foreign import showNumberAsInt :: Number -> String
 
 -- | Formats a number according to the format object provided. 
