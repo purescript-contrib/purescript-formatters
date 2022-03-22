281fc175ee2c76f657cc322f1e66c28d0213d6be
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-22 14:51:20 -0500

    Fix linting error

--- src/Data/Formatter/Number.js
+++ src/Data/Formatter/Number.js
@@ -2,4 +2,6 @@
 // (like `show` for `Number`) or clamping to +/- 2 billion (like when working
 // with `Int`). This is important for performance compared to other means of
 // showing an integer potentially larger than +/- 2 billion.
-export const showNumberAsInt = (n) => Math.round(n).toString()
+export function showNumberAsInt(n) {
+  return Math.round(n).toString();
+}
