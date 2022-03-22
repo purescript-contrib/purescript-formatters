d8bf52fc3e3e93ddfa66b2acccc27f9659ad43cb
Jordan Martinez <jordanalex.martinez@protonmail.com>
2022-03-22 09:21:04 -0500

    Removed '"use strict";' in FFI files

--- src/Data/Formatter/Number.js
+++ src/Data/Formatter/Number.js
@@ -1,5 +1,3 @@
-"use strict";
-
 // Converts a number to a string of the nearest integer _without_ appending ".0"
 // (like `show` for `Number`) or clamping to +/- 2 billion (like when working
 // with `Int`). This is important for performance compared to other means of
